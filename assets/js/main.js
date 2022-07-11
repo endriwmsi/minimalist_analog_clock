9a4646ea179bff5e3a6768ae6e86165bc449ba7a
Endriw Schiavenato <endriwm.schiavenato@rede.ulbra.br>
2022-07-11 19:17:56 -0300

    Corrigido mostrador de horas, opção de habilitar formato 12h e dark/light mode

--- assets/js/main.js
+++ assets/js/main.js
@@ -19,14 +19,14 @@ setInterval(clock, 1000) // 1000 = 1seg
 
 /* Texto da Hora e data */
 const textHour = document.getElementById('text-hour'),
-      textMinute = document.getElementById('text-minutes'),
+      textMinutes = document.getElementById('text-minutes'),
       textAmPm = document.getElementById('text-ampm'),
       dateDay = document.getElementById('date-day'),
       dateMonth = document.getElementById('date-month'),
       dateYear = document.getElementById('date-year')
 
 const clockText = () => {
-    let date = new date()
+    let date = new Date()
 
     let hh = date.getHours(),
     ampm,
@@ -35,20 +35,61 @@ const clockText = () => {
     month = date.getMonth (),
     year = date.getFullYear ()
 
-    /* Trocando de 12 para 24 */
+    /* Trocando formato de 24 para 12 */
+        // if(hh >= 12){
+        //      hh = hh - 12
+        //      ampm = 'PM'
+        // }else{
+        //      ampm = 'AM'
+        // }
+        // if(hh == 0){hh = 12}
 
-    // if( hh >= 12){
-    //     hh =  - 12
-    //     ampm = 'PM'
-    // }else{
-    //     ampm = 'AM'
-    // }
+    /* Mostrar 0 depois das horas */
+        // if(hh < 10){hh = `0${hh}`}
 
-    if(hh == 0){hh = 12}
+    /* Mostrar 0 depois dos minutos */
+        if(mm < 10){mm = `0${mm}`}
 
-    if(hh < 10){hh == `0${hh}`}
-
+    /* Mostrar as horas */
     textHour.innerHTML = `${hh}:`
-    textMinute.innerHTML = mm
+    textMinutes.innerHTML = mm
+
+    /* Mostrar AM/PM */
+    // textAmPm.innerHTML = ampm
+
+    /* Mostrar dia da Semana */
+    let week = ['Sun', 'Mon', 'Tues', 'Wed', 'Thurs', 'Fri', 'Sat'];
+
+    /* Mostrar Mês do ano */
+    let months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'];
+
+    /* Mostrar dia, mês e ano */
+    dateDay.innerHTML = day
+    dateMonth.innerHTML = `${months[month]},`
+    dateYear.innerHTML = year
 }
 setInterval(clockText, 1000)
+
+/* Dark/Light mode */
+const themeButton = document.getElementById('theme-button');
+const darkTheme = 'dark-theme';
+const iconTheme = 'bxs-sun';
+
+const selectedTheme = localStorage.getItem('selected-theme');
+const selectedIcon = localStorage.getItem('selected-icon');
+
+const getCurrentTheme = () => document.body.classList.contains(darkTheme) ? 'dark' : 'light';
+const getCurrentIcon = () => document.body.classList.contains(iconTheme) ? 'bxs-moon' : 'bxs-sun';
+
+if(selectedTheme){
+    document.body.classList[selectedTheme === 'dark' ? 'add' : 'remove'](darkTheme);
+    themeButton.classList[selectedIcon === 'bxs-moon' ? 'add' : 'remove'](iconTheme);
+}
+
+themeButton.addEventListener('click', () => {
+    document.body.classList.toggle(darkTheme);
+    themeButton.classList.toggle(iconTheme);
+
+    localStorage.setItem('selected-theme', getCurrentTheme);
+    localStorage.setItem('selected-icon', getCurrentIcon);
+})
